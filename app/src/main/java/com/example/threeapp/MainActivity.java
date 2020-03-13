a8ad96be32399bf6a065428281ef30c37b3eee9e
乔nameqiaoweiming <1330027991@qq.com>
2020-03-13 17:07:37 +0800

    add

--- app/src/main/java/com/example/threeapp/MainActivity.java
+++ app/src/main/java/com/example/threeapp/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //add something......
+
 
     }
 }
